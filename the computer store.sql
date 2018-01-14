9d6286019a7f47530442dcfe267174ee8fe0fd27
hiteshn97 <hiteshnankani97@gmail.com>
2018-01-14 13:23:04 +0530

    Completed.

--- the computer store.sql
+++ the computer store.sql
@@ -18,6 +18,40 @@ Select Avg (Price) from Products where Manufacturer = 2;
 
 Select Count(*) from Products where Price >= 180;
 
-Select Name, Price from Products where Price >= 180 and order by Price desc, Name;
+Select Name, Price from Products where Price >= 180 ana	d order by Price desc, Name;
 
 Select * from Products, Manufacturers where Products.Manufacturer = Manufacturers.Code
+
+Select Products.Name, Price, Manufacturers.Name from Products,Manufacturers where Products.Manufacturer = Manufacturers.Code
+
+Select Avg(Price), Manufacturers.Name
+	from Products, Manufacturers 
+	where Products.Manufacturer = Manufacturers.Name
+	group by Manufacturers.Name ;
+
+Select Avg(Price), Manufacturers.Name from Products, Manufacturers 
+	where Products.Manufacturer = Manufacturers.Name
+	group by Manufacturers.Name
+	having Avg(Price) >= 150;
+
+Select Name, Price from Products where Price = Min(Price);
+
+
+# 16. This one was tricky.
+# Select the name of each manufacturer along with the name and price of its most expensive product.
+Select M.Name, P.Name,Price
+	from Manufacturers M, Products P
+	where M.Code = P.Manufacturer
+	and P.Price = 
+	(
+		Select Max(Products.Price) from Products
+		where Products.Manufacturer = M.code
+	);
+
+Insert into Products( Name, Price, Manufacturer) values ('Loudspeaker', 70,2);
+
+Update Products Set Name='Laser Printer' where Code = 8;
+
+Update Products Set Price = Price * (0.9)
+
+Update Products Set Price = Price * (0.9) where Price >= 120
